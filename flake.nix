d1405b4f3c3aacbb88795b206be09a019db2dd87
haruki7049 <tontonkirikiri@gmail.com>
2025-11-11 20:09:10 +0900

    build(flakes): Update

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     flake-compat.url = "github:edolstra/flake-compat";
+    nix-gleam.url = "github:arnarg/nix-gleam";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -17,37 +18,75 @@
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
+
       imports = [
         inputs.treefmt-nix.flakeModule
       ];
 
       perSystem =
-        { pkgs, ... }:
         {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs.nixfmt.enable = true;
-            programs.taplo.enable = true;
-            programs.actionlint.enable = true;
-
-            settings.formatter.taplo.excludes = [
-              "manifest.toml"
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          app = pkgs.buildGleamApplication {
+            src = lib.cleanSource ./.;
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.nix-gleam.overlays.default
             ];
           };
 
-          devShells.default = pkgs.mkShell {
-            packages = [
-              # Nix
-              pkgs.nil
+          treefmt = {
+            projectRootFile = ".git/config";
 
-              # Gleam
-              pkgs.gleam
-              pkgs.erlang
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # Gleam
+            programs.gleam.enable = true;
+
+            # TOML
+            programs.taplo.enable = true;
+            settings.formatter.taplo.excludes = [
+              "manifest.toml"
+              "*/manifest.toml"
             ];
 
-            shellHook = ''
-              export PS1="\n[nix-shell\w]$ "
-            '';
+            # GitHub Actions
+            programs.actionlint.enable = true;
+
+            # Markdown
+            programs.mdformat.enable = true;
+
+            # ShellScript
+            programs.shellcheck.enable = true;
+            programs.shfmt.enable = true;
+          };
+
+          packages = {
+            inherit app;
+            default = app;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              # Nix LSP
+              pkgs.nil
+
+              # Gleam-lang
+              pkgs.gleam
+              pkgs.erlang
+              #pkgs.rebar3
+              #pkgs.elixir
+              #pkgs.deno
+            ];
           };
         };
     };
